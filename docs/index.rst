698ee54eaaee8610a3a1bf6c247044c83e2dc7e2
Chris Williamson <77583247+ch-williamson@users.noreply.github.com>
2022-09-20 10:11:35 +0100

    Cookiecutter setup (#2)
    Co-authored-by: Chris Williamson <chris.williamson@nesta.org.uk>

---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+
+   api/asf_senedd_response
+   License <license>
+
+asf_senedd_response
+===============================
+
+Analysis for A Sustainable Future's response to the Welsh Senedd's call for evidence on decarbonising homes s in August 2022.
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,26 @@
+"""asf_senedd_response."""
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read_lines(path):
+    """Read lines of `path`."""
+    with open(path) as f:
+        return f.read().splitlines()
+
+
+BASE_DIR = Path(__file__).parent
+
+
+setup(
+    name="asf_senedd_response",
+    long_description=open(BASE_DIR / "README.md").read(),
+    install_requires=read_lines(BASE_DIR / "requirements.txt"),
+    extras_require={"dev": read_lines(BASE_DIR / "requirements_dev.txt")},
+    packages=find_packages(exclude=["docs"]),
+    version="0.1.0",
+    description="Analysis for A Sustainable Future's response to the Welsh Senedd's call for evidence on decarbonising homes s in August 2022.",
+    author="Nesta",
+    license="MIT",
+)
