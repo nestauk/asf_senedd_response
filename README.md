64fa00c1365de1774c9f4aad96b86caf5a54d256
Chris Williamson <77583247+ch-williamson@users.noreply.github.com>
2023-06-07 15:15:22 +0100

    April 2023 consultation response (reworked old charts) (#3)

--- README.md
+++ README.md
@@ -1,12 +1,51 @@
 # asf_senedd_response
 
+This repo contains code for producing charts for ASF's August 2022 response to the Welsh Government call for evidence on home heating, and the updated versions of these plots for ASF's April 2023 response to the Welsh Government's consultation on Wales' renewable energy targets. The remainder of the charts for the April 2023 reponse can be produced from code in the repo `asf_welsh_energy_consultation`, as these charts are brand new for the response and this repo contains reworkings of older charts.
+
 ## Setup
 
 - Meet the data science cookiecutter [requirements](http://nestauk.github.io/ds-cookiecutter/quickstart), in brief:
   - Install: `direnv` and `conda`
-- Run `make install` to configure the development environment:
-  - Setup the conda environment
-  - Configure `pre-commit`
+- Clone the repo: `git clone git@github.com:nestauk/asf_senedd_response.git`
+- Navigate to the repo folder
+- Run `direnv allow`
+- Checkout the correct branch if not working on dev
+- Try `make install`, but this old repo predates some cookiecutter functionality so it might not work. If it doesn't then just create the conda environment manually: `conda create --name asf_senedd_response python=3.8`, `conda activate asf_senedd_response`, `pip install -r requirements.txt`, `pip install -r requirements_dev.txt`
+- Activate the conda environment if it is not already: `conda activate asf_senedd_response`
+
+- Perform additional setup in order to save plots:
+
+  - Follow the instructions (here)[https://github.com/altair-viz/altair_saver/#nodejs] - you may just need to run `conda install -c conda-forge vega-cli vega-lite-cli`
+
+- Change `LOCAL_DATA_DIR` in `getters/loading.py` to your local EPC data directory.
+
+- Run `python asf_senedd_response/analysis/wales_analysis.py`. This should generate 10 plots, five in each of `outputs/figures/english/` and `outputs/figures/welsh/`:
+  - `age_prop.png`
+  - `epc_all.html`
+  - `epc_hp_private_retrofit.html`
+  - `epc_hp_private.html`
+  - `hp_tenure.html`
+
+Note: no syncing of data from S3 is required as only EPC data is used here.
+
+## Skeleton folder structure
+
+```
+asf_senedd_response/
+├─ analysis/
+│  ├─ wales_analysis.py - produces plots and stats
+├─ getters/
+│  ├─ loading.py - getters for raw data
+├─ pipeline/
+│  ├─ augmenting.py - functions to process and enhance raw data
+│  ├─ plotting.py - generic plotting functions
+├─ utils/
+│  ├─ formatting.py - formatting numbers for plots
+outputs/
+├─ figures/
+│  ├─ english/ - English-language charts
+│  ├─ welsh/ - Welsh-language charts
+```
 
 ## Contributor guidelines
 
---
+++ asf_senedd_response/getters/loading.py
@@ -0,0 +1,85 @@
+# File: asf_senedd_response/getters/loading.py
+"""
+For loading of raw data.
+"""
+
+from asf_core_data.getters.epc import epc_data
+import pandas as pd
+import os
+
+LOCAL_DATA_DIR = "/Users/chris.williamson/Documents/ASF_data/"
+input_data_path = "inputs/data/"
+wales_epc_path = "wales_epc.csv"
+
+
+def load_wales_df(from_csv=True):
+    """Load preprocessed and deduplicated EPC dataset for Wales.
+    If data is loaded from all-GB file, the filtered version is saved to csv
+    for easier future loading.
+
+    Args:
+        from_csv (bool, optional): Whether to load from saved CSV. Defaults to True.
+
+    Returns:
+        pd.DataFrame: EPC data.
+    """
+    if from_csv:
+        wales_epc = pd.read_csv(wales_epc_path)
+    else:
+        wales_epc = epc_data.load_preprocessed_epc_data(
+            data_path=LOCAL_DATA_DIR,
+            subset="Wales",
+            batch="newest",
+            version="preprocessed_dedupl",
+            usecols=[
+                "LMK_KEY",
+                "INSPECTION_DATE",
+                "UPRN",
+                "POSTCODE",
+                "CURRENT_ENERGY_EFFICIENCY",
+                "CURRENT_ENERGY_RATING",
+                "WALLS_ENERGY_EFF",
+                "FLOOR_ENERGY_EFF",
+                "ROOF_ENERGY_EFF",
+                "CONSTRUCTION_AGE_BAND",
+                "TENURE",
+                "TRANSACTION_TYPE",
+                "HP_INSTALLED",
+            ],
+        )
+
+        wales_epc.TENURE = wales_epc.TENURE.replace(
+            {
+                "owner-occupied": "Owner-occupied",
+                "rental (social)": "Socially rented",
+                "rental (private)": "Privately rented",
+                "unknown": "Unknown",
+            }
+        )
+        # if CONSTRUCTION_AGE_BAND is unknown and TRANSACTION_TYPE is new dwelling,
+        # assume construction age is >2007 because EPCs started in 2008
+        wales_epc["CONSTRUCTION_AGE_BAND"].loc[
+            (wales_epc.CONSTRUCTION_AGE_BAND == "unknown")
+            & (wales_epc.TRANSACTION_TYPE == "new dwelling")
+        ] = "2007 onwards"
+
+        if not os.path.isdir(input_data_path):
+            os.makedirs(input_data_path)
+
+        wales_epc.to_csv(input_data_path + wales_epc_path)
+
+    return wales_epc
+
+
+def load_wales_hp(wales_epc):
+    """Load Welsh EPC data filtered to properties with heat pumps.
+
+    Args:
+        wales_epc (pd.DataFrame): Wales EPC data.
+
+    Returns:
+        pd.DataFrame: EPC data filtered to properties with heat pumps.
+    """
+    wales_hp = wales_epc.loc[wales_epc.HP_INSTALLED].reset_index(drop=True)
+
+    return wales_hp
